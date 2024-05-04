d18c0a9745844aa3a3c805fbc41c074d9ce8b258
oodball <74622107+oodball@users.noreply.github.com>
2024-05-04 12:42:36 -0700

    push units

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Anny Zhao & Audrey Liang
 [Link to Expose](https://oodball.github.io/Lab5_Starter/expose.html)
 
 [Link to Explore](https://oodball.github.io/Lab5_Starter/explore.html)
+
+
+## Pt3. Unit Testing with Jest
+- No, the cons of a unit test are that it can't be tested based on features. Messages can change based on the user, so it's harder. 
+- Yes, it would be useful to use a unit test, because there aren't many moving parts to it and it can be tested easily. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,4 @@
 // unit.test.js
-
 import {
   isPhoneNumber,
   isEmail,
@@ -8,4 +7,52 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+// // isPhoneNumber Tests - True
+// test('phone number is (555) 555-5555', () => {
+//   expect(isPhoneNumber('(555) 555-5555')).toBe(true);
+// });
+
+// // isPhoneNumber Tests - True
+// test('phone number is (111)111-1111', () => {
+//   expect(isPhoneNumber('(111)111-1111')).toBe(true);
+// });
+
+
+// // isPhoneNumber Tests - false
+// test('phone number is (55) 5555555', () => {
+//   expect(isPhoneNumber('(55) 5555555')).toBe(false);
+// });
+
+// // isPhoneNumber Tests - false
+// test('phone number is 143455555905', () => {
+//   expect(isPhoneNumber('143455555905')).toBe(false);
+// });
+
+//--------------------------------------------------------------------------------
+
+//isEmail Tests - true
+test('email is ay@rocket.com', () => {
+  expect(isEmail('ay@rocket.com')).toBe(true);
+});
+
+//isEmail Tests - true
+test('email is rawr@rawr.com', () => {
+  expect(isEmail('rawr@rawr.com')).toBe(true);
+});
+
+//isEmail Tests - false
+test('email is ay at rocket dot com', () => {
+  expect(isEmail('ayrocketcom')).toBe(false);
+});
+
+//isEmail Tests - false
+test('email is wow@', () => {
+  expect(isEmail('wow@')).toBe(false);
+});
+// --------------------------------------------------------------------------------
+
+test('password is ay@rocket.com', () => {
+  expect(isStrongPassword('ay@rocket.com')).toBe(true);
+});
+
+
